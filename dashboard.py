b444c68dae66b8d0bdfcb7529f1349d30eeb667c
yoshinrr <yoshinrambeloson@gmail.com>
2022-04-08 10:51:45 +0200

    maj

--- dashboard.py
+++ dashboard.py
@@ -62,7 +62,7 @@ def main():
         ['Total des revenus et des crédits', 'Dépenses en biens', 'Ration entre revenu et dépenses'])
     st.title("Informations client")
 
-    st.table((X.loc[client_id]).T)
+    st.table((X.loc[client_id])[0:8].T)
 
     if informations == 'Total des revenus et des crédits':
         if ind<20:
